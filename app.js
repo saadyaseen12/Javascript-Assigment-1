f474eee150e58c9317357a1e28abe37ac6382ca0
Saad Khan <155878793+saadyaseen12@users.noreply.github.com>
2024-01-21 11:28:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var age =25;
+var isstudent =true;
+
+let num1 = Number('Age')
+let num2 = Number('isstudent')
+
+
+{
+console.log("age:" + age);
+}
+{
+console.log("isstudent:" + isstudent);
+}
+
+
+
+var message = "Hello, welcome to my webpage! My name is Muhammad Saad Khan I am a webdeveloper. I can help you with varios tasks such as searching the web, generating the contant, and more.";
+alert(message);
